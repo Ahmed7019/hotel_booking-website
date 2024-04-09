3dc0620c8d9ddec2ce63346bed669c8566fb5c6d
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2024-04-09 10:28:35 +0200

    header done

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,24 @@ module.exports = {
       colors: {
         primaryBlue: {
           100: "#07689F",
+          400: "#2F83B2",
+          500: "#07689F",
+          600: "#043E5F",
+        },
+        neutral: {
+          100: "#F9F9F9",
+          200: "#EFEFEF",
+          300: "#D9D9D9",
+          400: "#A6A6A6",
+          500: "#565656",
+        },
+        stateColour: {
+          errL: "#E5062E",
+          errD: "#A80102",
+          alert: "#FFCC00",
+          accent: "#FF6C01",
+          success: "#4C9839",
+          successLight: "#E8F5E4",
         },
       },
     },
